7e78d0c34910ce816109fe6c6c3e9e4cd9414df8
Hugo Peres <hugo-pdf@hotmail.com>
2019-07-17 18:25:11 -0300

    New Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import  { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import routes from './routes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    
+  }
+
+  render() {
+
+    return (
+      <main className="App">
+        <NavBar />
+        <Switch>
+          {
+            routes.map((r, i) => (<Route key={i} exact path={r.path} component={r.component} />))
+          }
+        </Switch>
+      </main>
+    )
+  }  
 }
 
 export default App;
